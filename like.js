71e54fd831716e463c461530aab0539e3a3dde41
dpmasters <dmpanfilov@mail.ru>
2023-11-30 01:24:04 +0500

    check

--- like.js
+++ like.js
@@ -6,8 +6,9 @@ export function addLikeEventListeners() {
   likeButtons.forEach((likeButton, index) => {
     likeButton.addEventListener("click", (event) => {
       event.stopPropagation();
-      toggleLike({ id:comments[index].id }).then(() => {
-        getRenderComments();
+      toggleLike({ id:comments[==index].id }).then(() => {
+        getRender
+        __Comments();
       })
     });
   });
